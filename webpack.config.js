e6af3bd2ffff5132bc969d89eedd45932b41ff60
Kate <ekaterinavolkovaalex@gmail.com>
2018-05-31 14:21:09 +0300

    Add webpack.config.js file

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+let path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+let conf = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+    publicPath: 'dist/'
+  },
+  devServer: {
+    overlay: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        //exclude: '/node_modules/'
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          //fallback: "style-loader",
+          use: "css-loader"
+        })
+        // use: [
+        //   'style-loader',
+        //   'css-loader'
+        // ]
+
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("style.css"),
+  ]
+};
+
+module.exports = (env, options) => {
+  let production = options.mode === 'production';
+
+  conf.devtool = production ? 'source-map' : 'eval-sourcemap';
+  
+  return conf;
+}
