2d566cab2c5cb414b708cb228e47f35d6fbaa565
Kate <ekaterinavolkovaalex@gmail.com>
2018-06-02 14:00:12 +0300

    Hide landing page and create canvas by button click

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
 import '../css/style.css';
+
+let playButton = document.querySelector('.button-play');
+playButton.addEventListener('click', loadGame);
+
+function loadGame() {
+  document.body.innerHTML = '';
+  let canvas = document.createElement('canvas');
+  document.body.appendChild(canvas);
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  let c = canvas.getContext('2d');
+}
