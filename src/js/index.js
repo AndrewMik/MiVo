e67692d32565873ec4deb53f386cec2c6de07162
Kate <ekaterinavolkovaalex@gmail.com>
2018-06-06 16:41:08 +0300

    Add spell images; Delete unused images; Implement generateMonsterName func

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
-import '../css/style.css';
-const pathToImgs = require.context('../img', true);
+import "../css/style.css";
+const pathToImgs = require.context("../img", true);
 
-const playButton = document.querySelector('.button-play');
-playButton.addEventListener('click', loadGame);
+const playButton = document.querySelector(".button-play");
+playButton.addEventListener("click", loadGame);
 
 // Temporary loads the game
 // TODO: Remove in last version
-document.addEventListener('DOMContentLoaded', loadGame);
+document.addEventListener("DOMContentLoaded", loadGame);
 
 function loadGame() {
   hideLandingPage();
@@ -17,9 +17,6 @@ function loadGame() {
   // TODO: Remove in last version
   // toggleRegisterFieldVisibility();
   // startGame('Herooooo', 0);
-
-  toggleRegisterFieldVisibility();
-  startGame(0);
 }
 
 function generateMonster() {
@@ -37,14 +34,6 @@ function generateMonster() {
     Math.round(Math.random() * (bodiesNum + 1)) * 234 + "px 0";
   monster.appendChild(monsterHead);
   monster.appendChild(monsterBody);
-}
-
-function generateMonster() {
-  // Array for monster parts
-  // Now we use only one part for the whole body
-  let parts = [];
-  let monster = document.createElement('img');
-  monster.src = pathToImgs('./monster-1.png', true);
 
   return monster;
 }
@@ -53,16 +42,16 @@ function showHeroes(hero, monster) {
   let heroContainer = document.querySelector(".hero");
   // heroContainer.appendChild(hero);
 
-  let monsterContainer = document.querySelector('.monster');
+  let monsterContainer = document.querySelector(".monster");
   monsterContainer.appendChild(monster);
 
-  heroContainer.classList.add('hero--appear');
-  monsterContainer.classList.add('monster--appear');
+  heroContainer.classList.add("hero--appear");
+  monsterContainer.classList.add("monster--appear");
 }
 
 function startGame(heroName, char, level = 1) {
-  let character = document.querySelector('.hero');
-  character.style.backgroundPosition = -(12 - char) * 267 + 'px 0';
+  let character = document.querySelector(".hero");
+  character.style.backgroundPosition = -(12 - char) * 267 + "px 0";
   let monster = generateMonster();
 
   showHeroes(character, monster);
@@ -77,7 +66,7 @@ function startGame(heroName, char, level = 1) {
   setHeroNames(heroName, monsterName);
 
   setTimeout(showFightBox, 2000);
-  setTimeout( () => {
+  setTimeout(() => {
     hideFightBox();
     setTimeout(chooseSpell, 1000);
     //Temporary shows and hides the modal dialogue to choose a spell
@@ -106,7 +95,53 @@ function setHeroNames(heroName, monsterName) {
 }
 
 function generateMonsterName() {
-  return "Monsterrrrrrr";
+  
+  let db = {
+    'adj': 
+    [
+      'сопливый', 'малый', 'синий', 'очумевший', 'светящийся', 'злобный', 'вонючий', 'летающий', 'пустынный', 
+      'одинокий', 'пятнистый', 'ужасный', 'одноглазый', 'свирепый', 'костлявый', 'хилый', 'косой', 'грозный',
+      'беспощадный', 'трусливый', 'серый', 'бессмертный', 'древний', 'черный', 'двуликий', 'мутный', 'слепой', 
+      'зубастый', 'страшный', 'плоскомордый', 'ярый', 
+    ],
+    'root': 
+    [
+      // #0
+      'туман', 'леший', 'огр', 'гном', 'гоблин', 'луч', 'гремлин', 'металл', 'зомби', 'тигр', 
+      'вурдалак', 'неряха', 'забияка', 'волк', 'змей', 'призрак', 'упырь', 'крот', 'клык', 'бузак', 
+      'Стужник', 'механоид', 'иглоид', 'слизень', 'прыгун', 'ползун', 'ветрила', 'дикарь', 'рыб', 
+      'клыкан', 'хвостан', 'зубан', 'стенолом', 'гривослон', 'жаб', 'бомж', 'рог', 'кревет', 'хвост', 'свин', 'крыс', 
+      'шум', 'монстр', 'клюв', 'свет', 'дрон', 'шторм', 
+      // '', '', '', '', '', '', 
+      // '', '', '', '', '', '', '', '', '', '', '', 
+      // '', '', '', '', '', '', '', '', ''
+    ],
+    'name': 
+    [
+      'Том', 'Илон', 'Макс', 'Юрий', 'Петя', 'Фдам', 'Ефим', 'Алекс', 'Федя', 'Нил', 'Дэн', 'Ник', 'Роб', 'Миша', 
+      'Аарон', 'Арсений', 'Дэвид', 'Руслан', 'Артур', 'Марк', 'Глеб', 'Егор', 'Илья', 'Клим', 'Ваня', 'Тим', 'Боб', 
+      'Ян', 'Захар', 'Вадим', 'Коля', 'Георг', 'Макар', 'Игорь', 'Боря', 'Доминик', 'Ашот', 'Гавр', 'Гена'
+    ],
+  };
+
+  let name = '';
+  let i = 0;
+  let j = 0;
+  let k = 0;
+  
+  i = rand(0, db.adj.length - 1);
+  name += db.adj[i] + ' ';
+  j = rand(0, db.root.length - 1);
+  name += db.root[i] + ' ';
+  k = rand(0, db.name.length - 1);
+  name += db.name[i];
+  
+
+  function rand(min, max) { 
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  return name;
 }
 
 function setLevel(level) {
@@ -114,13 +149,13 @@ function setLevel(level) {
 }
 
 function showGameField() {
-  let gameField = document.querySelector('.game-container');
-  gameField.classList.remove('game-container--hidden');
+  let gameField = document.querySelector(".game-container");
+  gameField.classList.remove("game-container--hidden");
 }
 
 function hideLandingPage() {
-  let landing = document.querySelector('.landing-container');
-  landing.classList.add('landing-container--hidden');
+  let landing = document.querySelector(".landing-container");
+  landing.classList.add("landing-container--hidden");
 }
 
 function toggleRegisterFieldVisibility() {
@@ -135,8 +170,8 @@ function registerPlayer() {
   let mail;
   let char;
 
-  let form = document.querySelector('.register-form');
-  form.addEventListener('submit', (e) => {
+  let form = document.querySelector(".register-form");
+  form.addEventListener("submit", e => {
     e.preventDefault();
     login = document.getElementById("login").value;
     mail = document.getElementById("email").value;
@@ -148,32 +183,18 @@ function registerPlayer() {
   });
 }
 
-// this array can be used to make character consist of several parts
-// you have to add in array below array of sources to images
-// that will be different variants of a body part
-// you also have to duplicate .part-select element in index.html
-const CHARACTERS = [
-  ['./char1.gif',
-    './char2.gif',
-    './char3.gif']
-];
-
-
 function initCharacterSelectField() {
-
-  let characterSelect = document.querySelector('.character-select');
-  let partSelect = [...document.querySelectorAll('.part-select')];
-  let roleSliders = [...document.querySelectorAll('.role__slider')];
+  let characterSelect = document.querySelector(".character-select");
+  let partSelect = [...document.querySelectorAll(".part-select")];
+  let roleSliders = [...document.querySelectorAll(".role__slider")];
 
   roleSliders.forEach((slider, index) => {
-    // setParts(slider, CHARACTERS[index], index);
     setParts(slider, 13, index);
   });
 
-  partSelect.forEach((slider) => {
+  partSelect.forEach(slider => {
     setSliderEvents(slider);
   });
-
 }
 
 function setParts(slider, sources, index) {
@@ -185,10 +206,10 @@ function setParts(slider, sources, index) {
 
     let character = document.createElement("div");
     character.classList.add("slide__img");
-    character.style.backgroundPosition = (i+1) * 267 + "px 0";
+    character.style.backgroundPosition = (i + 1) * 267 + "px 0";
 
     if (i === 0) {
-      partSlide.classList.add('role__slide--active');
+      partSlide.classList.add("role__slide--active");
     }
 
     slider.appendChild(partSlide);
@@ -198,92 +219,118 @@ function setParts(slider, sources, index) {
 
 function setSliderEvents(slider) {
   // TODO: add listeners for keyboard
-  slider.addEventListener('click', slide);
+  slider.addEventListener("click", slide);
 }
 
 function slide(e) {
-
   let slider = this;
-  let slides = [...(slider.querySelector('.role__slider')).children];
+  let slides = [...slider.querySelector(".role__slider").children];
 
-  let slidesList = slider.querySelector('.role__slider');
-  let activeSlide = slider.querySelector('.role__slide--active');
+  let slidesList = slider.querySelector(".role__slider");
+  let activeSlide = slider.querySelector(".role__slide--active");
 
   let clickedButton = e.target;
 
   // PREVIOUS BUTTON
   // check if clicked button is 'previous button'
-  if (clickedButton.classList.contains('role__prev-button')) {
-
+  if (clickedButton.classList.contains("role__prev-button")) {
     // check if active indicator is the first one
     if (activeSlide === slidesList.firstElementChild) {
-
       // make the last slide active
       let lastSlide = slidesList.lastElementChild;
-      lastSlide.classList.add('role__slide--active');
+      lastSlide.classList.add("role__slide--active");
 
       // switch to the last slide
-      let lastSlideNumber = lastSlide.getAttribute('data-slide');
+      let lastSlideNumber = lastSlide.getAttribute("data-slide");
       slides.forEach(slide => {
         slide.style.transform = "translateX(" + lastSlideNumber * -267 + "px)";
       });
-
     } else {
       // make previous indicator active
       let prevSlide = activeSlide.previousElementSibling;
-      prevSlide.classList.add('role__slide--active');
+      prevSlide.classList.add("role__slide--active");
 
       // switch to previous slide
-      let prevSlideNumber = prevSlide.getAttribute('data-slide');
+      let prevSlideNumber = prevSlide.getAttribute("data-slide");
       slides.forEach(slide => {
         slide.style.transform = "translateX(" + prevSlideNumber * -267 + "px)";
       });
     }
 
-    activeSlide.classList.remove('role__slide--active');
+    activeSlide.classList.remove("role__slide--active");
   }
 
   // NEXT BUTTON
   // check if clicked button is 'next button'
-  if (clickedButton.classList.contains('role__next-button')) {
-
+  if (clickedButton.classList.contains("role__next-button")) {
     //check if active indicator is the last one
     if (activeSlide === slidesList.lastElementChild) {
-
       // make the first indicator active
       let firstSlide = slidesList.firstElementChild;
-      firstSlide.classList.add('role__slide--active');
+      firstSlide.classList.add("role__slide--active");
 
       // switch to the first slide
-      let firstSlideNumber = firstSlide.getAttribute('data-slide');
+      let firstSlideNumber = firstSlide.getAttribute("data-slide");
       slides.forEach(slide => {
         slide.style.transform = "translateX(" + firstSlideNumber * -267 + "px)";
       });
-
     } else {
-
       // make next indicator active
       let nextSlide = activeSlide.nextElementSibling;
-      nextSlide.classList.add('role__slide--active');
+      nextSlide.classList.add("role__slide--active");
 
       // switch to the next slide
-      let nextSlideNumber = nextSlide.getAttribute('data-slide');
+      let nextSlideNumber = nextSlide.getAttribute("data-slide");
       slides.forEach(slide => {
         slide.style.transform = "translateX(" + nextSlideNumber * -267 + "px)";
       });
     }
 
-    activeSlide.classList.remove('role__slide--active');
+    activeSlide.classList.remove("role__slide--active");
   }
 }
 
-//Modal dialogue - Choose a Spell
 function chooseSpell() {
   let modalChooseSpell = document.getElementById('modal-choose-spell');
 
-  toggleElementVisibitity(modalChooseSpell);
+  toggleElementVisibility(modalChooseSpell);
 
   document.body.addEventListener('click', checkModalChooseSpellClicked);
+}
+
+function hideModalChooseSpell() {
+  let modalChooseSpell = document.getElementById('modal-choose-spell');
+  let modalContentChooseSpell = document.getElementById('modal-content-choose-spell');
+
+  modalChooseSpell.classList.add('modal--hidden');
+
+  document.body.removeEventListener('click', checkModalChooseSpellClicked);
+}
+
+//Screen task
+function showTaskScreen() {
+  let taskScreen = document.getElementById('modal-screen-task');
+
+  toggleElementVisibility(taskScreen);
+}
+
+
+//Screen task
+function showTaskScreen() {
+  let taskScreen = document.getElementById('modal-screen-task');
+
+  toggleElementVisibility(taskScreen);
+}
+
+
+function hideTaskScreen() {
+  let taskScreen = document.getElementById('modal-screen-task');
+
+  taskScreen.classList.add('modal--hidden');
+}
+
+function toggleElementVisibility(element) {
+  element.classList.toggle("modal--hidden");
 }
 
 function checkModalChooseSpellClicked() {
@@ -304,30 +351,4 @@ function checkModalChooseSpellClicked() {
   if (!isModalContantClicked) {
     toggleElementVisibitity(modalChooseSpell);
   }
-}
-
-function hideModalChooseSpell() {
-  let modalChooseSpell = document.getElementById('modal-choose-spell');
-  let modalContentChooseSpell = document.getElementById('modal-content-choose-spell');
-
-  modalChooseSpell.classList.add('modal--hidden');
-
-  document.body.removeEventListener('click', checkModalChooseSpellClicked);
-}
-
-//Screen task
-function showTaskScreen() {
-  let taskScreen = document.getElementById('modal-screen-task');
-
-  toggleElementVisibitity(taskScreen);
-}
-
-function hideTaskScreen() {
-  let taskScreen = document.getElementById('modal-screen-task');
-
-  taskScreen.classList.add('modal--hidden');
-}
-
-function toggleElementVisibitity(element) {
-  element.classList.toggle('modal--hidden');
 }
